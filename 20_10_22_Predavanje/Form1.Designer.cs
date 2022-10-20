3eefdc77e0ac5049b3c6ba16d89c41402c066008
Barbara Friščić <80264267+BarbaraFriscic@users.noreply.github.com>
2022-10-20 16:21:41 +0200

    Add project files.

---
+++ 20_10_22_Predavanje/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace _20_10_22_Predavanje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalculateTax = new System.Windows.Forms.Button();
+            this.lblCalculationRez = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblCalculationRez2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGiftCard = new System.Windows.Forms.Button();
+            this.lblGiftCardReturn = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnMovie = new System.Windows.Forms.Button();
+            this.lblMovieResult = new System.Windows.Forms.Label();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.lblMultiplyReturn = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUserInput = new System.Windows.Forms.TextBox();
+            this.btnNumberSwitch = new System.Windows.Forms.Button();
+            this.btnStringSwitch = new System.Windows.Forms.Button();
+            this.lblSwitchReturn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pritisnite dugme za poziv metoda:";
+            // 
+            // btnCalculateTax
+            // 
+            this.btnCalculateTax.Location = new System.Drawing.Point(149, 100);
+            this.btnCalculateTax.Name = "btnCalculateTax";
+            this.btnCalculateTax.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculateTax.TabIndex = 1;
+            this.btnCalculateTax.Text = "OK";
+            this.btnCalculateTax.UseVisualStyleBackColor = true;
+            this.btnCalculateTax.Click += new System.EventHandler(this.btnCalculateTax_Click);
+            // 
+            // lblCalculationRez
+            // 
+            this.lblCalculationRez.AutoSize = true;
+            this.lblCalculationRez.Location = new System.Drawing.Point(108, 149);
+            this.lblCalculationRez.Name = "lblCalculationRez";
+            this.lblCalculationRez.Size = new System.Drawing.Size(0, 13);
+            this.lblCalculationRez.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(494, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 65);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Demonstracija poziva metode sa:\r\n-jednim INT parametrom\r\n-tri INT parametra\r\n-pet" +
+    "INT parametara\r\n-jednim INT nizom(array)";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(536, 129);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "OK";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblCalculationRez2
+            // 
+            this.lblCalculationRez2.AutoSize = true;
+            this.lblCalculationRez2.Location = new System.Drawing.Point(494, 167);
+            this.lblCalculationRez2.Name = "lblCalculationRez2";
+            this.lblCalculationRez2.Size = new System.Drawing.Size(0, 13);
+            this.lblCalculationRez2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Pritisnite OK za testiranje metode";
+            // 
+            // btnGiftCard
+            // 
+            this.btnGiftCard.Location = new System.Drawing.Point(111, 267);
+            this.btnGiftCard.Name = "btnGiftCard";
+            this.btnGiftCard.Size = new System.Drawing.Size(75, 23);
+            this.btnGiftCard.TabIndex = 7;
+            this.btnGiftCard.Text = "OK";
+            this.btnGiftCard.UseVisualStyleBackColor = true;
+            this.btnGiftCard.Click += new System.EventHandler(this.btnGiftCard_Click);
+            // 
+            // lblGiftCardReturn
+            // 
+            this.lblGiftCardReturn.AutoSize = true;
+            this.lblGiftCardReturn.Location = new System.Drawing.Point(81, 305);
+            this.lblGiftCardReturn.Name = "lblGiftCardReturn";
+            this.lblGiftCardReturn.Size = new System.Drawing.Size(0, 13);
+            this.lblGiftCardReturn.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(296, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Pritisnite OK za testiranje metoda";
+            // 
+            // btnMovie
+            // 
+            this.btnMovie.Location = new System.Drawing.Point(299, 266);
+            this.btnMovie.Name = "btnMovie";
+            this.btnMovie.Size = new System.Drawing.Size(75, 23);
+            this.btnMovie.TabIndex = 10;
+            this.btnMovie.Text = "OK";
+            this.btnMovie.UseVisualStyleBackColor = true;
+            this.btnMovie.Click += new System.EventHandler(this.btnMovie_Click);
+            // 
+            // lblMovieResult
+            // 
+            this.lblMovieResult.AutoSize = true;
+            this.lblMovieResult.Location = new System.Drawing.Point(299, 315);
+            this.lblMovieResult.Name = "lblMovieResult";
+            this.lblMovieResult.Size = new System.Drawing.Size(0, 13);
+            this.lblMovieResult.TabIndex = 11;
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(582, 232);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiply.TabIndex = 12;
+            this.btnMultiply.Text = "TEST";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // lblMultiplyReturn
+            // 
+            this.lblMultiplyReturn.AutoSize = true;
+            this.lblMultiplyReturn.Location = new System.Drawing.Point(533, 272);
+            this.lblMultiplyReturn.Name = "lblMultiplyReturn";
+            this.lblMultiplyReturn.Size = new System.Drawing.Size(0, 13);
+            this.lblMultiplyReturn.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(793, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(240, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Unesite vrijednost i pritisnite odgovarajuće dugme";
+            // 
+            // txtUserInput
+            // 
+            this.txtUserInput.Location = new System.Drawing.Point(817, 83);
+            this.txtUserInput.Name = "txtUserInput";
+            this.txtUserInput.Size = new System.Drawing.Size(190, 20);
+            this.txtUserInput.TabIndex = 15;
+            // 
+            // btnNumberSwitch
+            // 
+            this.btnNumberSwitch.Location = new System.Drawing.Point(817, 129);
+            this.btnNumberSwitch.Name = "btnNumberSwitch";
+            this.btnNumberSwitch.Size = new System.Drawing.Size(75, 23);
+            this.btnNumberSwitch.TabIndex = 16;
+            this.btnNumberSwitch.Text = "Broj";
+            this.btnNumberSwitch.UseVisualStyleBackColor = true;
+            this.btnNumberSwitch.Click += new System.EventHandler(this.btnNumberSwitch_Click);
+            // 
+            // btnStringSwitch
+            // 
+            this.btnStringSwitch.Location = new System.Drawing.Point(932, 129);
+            this.btnStringSwitch.Name = "btnStringSwitch";
+            this.btnStringSwitch.Size = new System.Drawing.Size(75, 23);
+            this.btnStringSwitch.TabIndex = 17;
+            this.btnStringSwitch.Text = "String";
+            this.btnStringSwitch.UseVisualStyleBackColor = true;
+            this.btnStringSwitch.Click += new System.EventHandler(this.btnStringSwitch_Click);
+            // 
+            // lblSwitchReturn
+            // 
+            this.lblSwitchReturn.AutoSize = true;
+            this.lblSwitchReturn.Location = new System.Drawing.Point(796, 187);
+            this.lblSwitchReturn.Name = "lblSwitchReturn";
+            this.lblSwitchReturn.Size = new System.Drawing.Size(0, 13);
+            this.lblSwitchReturn.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1171, 450);
+            this.Controls.Add(this.lblSwitchReturn);
+            this.Controls.Add(this.btnStringSwitch);
+            this.Controls.Add(this.btnNumberSwitch);
+            this.Controls.Add(this.txtUserInput);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblMultiplyReturn);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.lblMovieResult);
+            this.Controls.Add(this.btnMovie);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblGiftCardReturn);
+            this.Controls.Add(this.btnGiftCard);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblCalculationRez2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCalculationRez);
+            this.Controls.Add(this.btnCalculateTax);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalculateTax;
+        private System.Windows.Forms.Label lblCalculationRez;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblCalculationRez2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGiftCard;
+        private System.Windows.Forms.Label lblGiftCardReturn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnMovie;
+        private System.Windows.Forms.Label lblMovieResult;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Label lblMultiplyReturn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUserInput;
+        private System.Windows.Forms.Button btnNumberSwitch;
+        private System.Windows.Forms.Button btnStringSwitch;
+        private System.Windows.Forms.Label lblSwitchReturn;
+    }
+}
+
---
+++ 20_10_22_Predavanje/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20_10_22_Predavanje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculateTax_Click(object sender, EventArgs e)
+        {
+            double cijena = 79.95;
+            double poreznaStopa = 0.06;
+
+
+            IzracunPoreza(cijena, poreznaStopa);
+            IzracunPoreza(cijena);
+
+
+        }
+
+        public void IzracunPoreza(double cijena, double poreznaStopa = 0.06)
+        {
+
+            double porez = cijena * poreznaStopa;
+            lblCalculationRez.Text += String.Format("\n {0} poreza na {1} iznosi {2:C}.", poreznaStopa, cijena, porez);
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            int[] brojeviNiz = { 9, 5, 3, 77, 34, 65, 12, 3, 92, 4 }; //znači ako ćeš odmah napuniti niz, ne treba ono new int[]
+
+            Suma(5); //mogu se spremiti i u varijablu pa pozvati varijablu, ali varijabla mora biti vrste koja se poziva kao parametar
+            Suma(5, 12, 2);
+            Suma(5, 12, 2, 92, 154);
+            Suma(brojeviNiz);
+
+
+        }
+        public void Suma(params int[] parametri)
+        {
+            int suma = 0;
+            foreach (int par in parametri)
+            {
+                suma += par;
+            }
+            lblCalculationRez2.Text += String.Format("\n Suma iznosi {0}", suma);
+        }
+
+        private void btnGiftCard_Click(object sender, EventArgs e)
+        {
+            double iznos = 1250.75;
+            GiftCard(iznos);
+            GiftCard();
+
+        }
+        public void GiftCard(double iznos = 500)
+        {
+            lblGiftCardReturn.Text += String.Format("\nIznos na vašoj poklon kartici je {0:C}", iznos);
+        }
+
+        private void btnMovie_Click(object sender, EventArgs e)
+        {
+            Movie("Batman", 195);
+            Movie("James Bond");
+        }
+        public void Movie(string ime, int trajanje = 90)
+        {
+            lblMovieResult.Text += String.Format("\nNaziv filma: {0} --- trajanje {1} minuta", ime, trajanje);
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            Multiply(5, 2);
+            Multiply(5);
+            Multiply(7.13, 4);
+            Multiply(7.13);
+            Multiply("Ana", 1);
+            Multiply("Ana");
+        }
+
+        public void Multiply(int a, int b = 3)
+        {
+            lblMultiplyReturn.Text += String.Format("\nDobiveni parametri: {0} i {1}, izlaz: {2}", a, b, a * b);
+        }
+        public void Multiply(double a, int b = 3)
+        {
+            lblMultiplyReturn.Text += String.Format("\nDobiveni parametri: {0} i {1}, izlaz: {2}", a, b, a * b);
+        }
+        public void Multiply(string a, int b = 3)
+        {
+            string c = "";
+            for (int i = 0; i < b; i++)
+            {
+                c += a;
+            }
+            lblMultiplyReturn.Text += String.Format("\nDobiveni parametri: {0} i {1}, izlaz: {2}", a, b, c);
+        }
+
+        private void btnNumberSwitch_Click(object sender, EventArgs e)
+        {
+           
+            if (txtUserInput.Text == "")
+            { MessageBox.Show("Unesite dvoznamenkasti broj"); 
+            
+            }
+
+
+            int userInputNumber;
+            
+
+            
+            Zamjena(out userInputNumber);
+            
+        }
+
+        private void btnStringSwitch_Click(object sender, EventArgs e)
+        {
+            string userInputText;
+
+            if (txtUserInput.Text == "")
+            
+                MessageBox.Show("Unesite neki tekst");
+
+                Zamjena(out userInputText);
+        }
+
+        public void Zamjena(out int number)
+        {
+            number = Convert.ToInt32(txtUserInput.Text);
+            
+            string numberSwitch = "";
+            char[] wordChars = number.ToString().ToCharArray();
+
+            for (int i = number.ToString().Length - 1; i >= 0; i--)
+            {
+                numberSwitch += number.ToString()[i];                                      
+            }
+          
+            lblSwitchReturn.Text += String.Format("\nBroj: {0} -- Zamjena: {1}", number, numberSwitch);
+        }
+
+        public void Zamjena(out string word)
+        {
+            word = txtUserInput.Text;
+            string wordSwitch = "";
+            char[] wordChars = word.ToCharArray();
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                wordSwitch += wordChars[i];
+            }
+
+            //string wordSwitch = word.Substring(word.Length -1, 0);
+            
+            lblSwitchReturn.Text += String.Format("\nTekst: {0} -- Zamjena: {1}", word, wordSwitch);
+
+        }
+
+        
+    }
+}
